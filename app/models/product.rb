5fb4ba590aa1634a8d67d83253625314341405a4
Heber Lazo <heberlazo@gmail.com>
2021-12-12 18:56:41 -0500

    removed unneceary code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
 
-  def self.selectCategory(category)
-    where('category = ?', category)
-  end
-
   def self.selectedRandomDiscount
     where('discount > 5').order('RAND()').limit(3)
   end
