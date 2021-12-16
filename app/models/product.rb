07c64b023cfec7e5dacb640cc1d09b05327f26be
Heber Lazo <heberlazo@gmail.com>
2021-12-16 11:31:07 -0500

    Refactor code  in query

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,7 @@ class Product < ApplicationRecord
     return [] unless price.to_i
     real_price = price.to_i * 1000
     
-    if discount == 'false'
-      where('discount = 0 AND price <= ?', real_price)
-    else
-      where('discount > 0 AND price <= ?', real_price)
-    end
+      discount == 'false' ? where('discount = 0 AND price <= ?', real_price) : where('discount > 0 AND price <= ?', real_price)
 
   end
 
@@ -22,7 +18,6 @@ class Product < ApplicationRecord
     }
 
       if params[:name] 
-      puts params[:name]    
         #name and cat
         if params[:categories]
           where("name ILIKE ? AND category_id In (?) ",  "%#{params[:name]}%", params[:categories])
